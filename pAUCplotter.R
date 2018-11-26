1d6592ee32d21caade62ac091b7e92360ce9860e
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-11-26 14:26:53 -0500

    Return plot function in pAUCplotter list

--- pAUCplotter.R
+++ pAUCplotter.R
@@ -11,17 +11,19 @@ pAUCplotter <- function(response, predictor, partial.auc = c(1, .8), partial.auc
    smoothed <- smooth(rocFit, method="density")
    
    
-   plot(0, 0, type="n", xlab="1 - Specificity", ylab="Sensitivity", main=mainTitle, xlim=c(0,1), ylim=c(0,1))
-   # note: this piece only works for focus == "specificity" right now
-   polygon(list(x = 1 - c(partial.auc, rev(partial.auc)), y=c(0,0,1,1)), col="lightgray", lwd=.5)
-   lines(1-smoothed$sp, smoothed$se, col="darkorange2", lwd=2)
-   t1ind <- which.min(abs(smoothed$sp-.8))
-   polyvec <- list(x=c(1-smoothed$sp[length(smoothed$sp):t1ind], .2),
-                   y=c(smoothed$se[length(smoothed$se):t1ind], 0))
-   polygon(polyvec, col="darkblue", density=15, angle=125)
-   abline(0, 1, lty=2, col="darkgray")
+   p <- function() {
+      plot(0, 0, type="n", xlab="1 - Specificity", ylab="Sensitivity", main=mainTitle, xlim=c(0,1), ylim=c(0,1))
+      # note: this piece only works for focus == "specificity" right now
+      polygon(list(x = 1 - c(partial.auc, rev(partial.auc)), y=c(0,0,1,1)), col="lightgray", lwd=.5)
+      lines(1-smoothed$sp, smoothed$se, col="darkorange2", lwd=2)
+      t1ind <- which.min(abs(smoothed$sp-.8))
+      polyvec <- list(x=c(1-smoothed$sp[length(smoothed$sp):t1ind], .2),
+                      y=c(smoothed$se[length(smoothed$se):t1ind], 0))
+      polygon(polyvec, col="darkblue", density=15, angle=125)
+      abline(0, 1, lty=2, col="darkgray")
+   }
    
-   return(list(aucEst = aucEst, aucCI = aucCI, pAucEst = pAucEst, pAucCI = pAucCI))
+   list(aucEst = aucEst, aucCI = aucCI, pAucEst = pAucEst, pAucCI = pAucCI, plot = p)
 }
 
 ### test the function
@@ -34,13 +36,14 @@ x1offset <- c(rbeta(ncases, .1, .1), rbeta(ncontrols, .5, 100))
 x1 <- as.numeric(scale(x1offset + rnorm(ncases+ncontrols, sd=.3)))
 
 # plot
+fit <- pAUCplotter(y, x1, boot.n = 100)
 pdf("figure1.pdf", width=7, height=7)
-pAUCplotter(y, x1, boot.n = 100)
+fit$plot()
 dev.off()
 
 # plot with extra annotations
 pdf("figure2.pdf", width=7, height=7)
-fit <- pAUCplotter(y, x1, boot.n = 100) 
+fit$plot()
 text(x=.9, y=.05, labels=paste("AUC =", format(round(fit$aucEst, 2), nsmall=2)))
 text(x=.9, y=0, labels=substitute(paste('pAUC'['(0,0.2)'], ' = ', pAucEst), 
                                   list(pAucEst=format(round(fit$pAucEst, 2), nsmall=2, digits=2))))
